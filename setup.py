d4cabfeafec9273ed5b83d953f27fb6cd02d549c
simone1999 <simon@klein-homepage.de>
2019-09-09 17:27:57 +0200

    added setup.py for pip

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='Tetris_kind_of_Gym',
+
+     version='0.1',
+
+     scripts=['Code/tetris', 'Code/tetris_moreNumpy'] ,
+
+     author="Klein Simon",
+
+     author_email="simon@klein-homepage.de",
+
+     description="fast paralell Tetris implementation for Reinforcement learning",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="https://github.com/simone1999/Tetris_kind_of_Gym",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         # "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
